837442df8c0b7755cec2fa9b56254f8b4ede8dbb
Admin <ahmed.bahloul.dev@gmail.com>
2025-11-14 12:10:59 +0100

    chore(frontend): migrate from javascript to typescript

--- tech-task-client/src/App.test.tsx
+++ tech-task-client/src/App.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { test, expect } from '@jest/globals';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeTruthy();
 });
